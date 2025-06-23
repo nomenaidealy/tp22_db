9750846023dca0968852f3d79d9a4da4a922e865
Sambatra-Mickaella <mickaella2341@gmail.com>
2025-06-23 16:38:21 +0300

    acceuil1

--- inc/fonction.php
+++ inc/fonction.php
@@ -11,6 +11,18 @@ function select_name_manager(){
     $sql="SELECT last_name,departments.dept_no,departments.dept_name from employees 
     join departments 
     join dept_manager 
+    on dept_manager.dept_no=departments.dept_no on dept_manager.emp_no=employees.emp_no
+    where dept_manager.to_date>NOW()";
+
+$sql=sprintf($sql);
+$resultat = mysqli_query(dbconnect(), $sql);
+return $resultat;
+}
+
+function select_lien_departement(){
+    $sql="SELECT last_name from employees 
+    join departments 
+    join dept_manager 
     on dept_manager.dept_no=departments.dept_no on dept_manager.emp_no=employees.emp_no";
 
 $sql=sprintf($sql);
